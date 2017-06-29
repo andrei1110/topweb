19e65dd84e9c1957d7dc05c92386c98497d586e0
andrei1110 <actoledo.andrei@gmail.com>
2017-06-29 15:23:58 -0300

    correços no proj1 e inclusao proj2
    Pequenas correções de design no proj 1 e inclusão do início do projeto
    2, ainda nada implementado no projeto 2

---
+++ projeto 2/projeto2/import/db.js
@@ -0,0 +1,6 @@
+//db.js
+//Create by Andrei Toledo
+//Tópicos especiais em desenvolvimento web
+
+import { Mongo } from 'meteor/mongo';
+Images = new Mongo.Collection("products");
---
+++ projeto 2/projeto2/server/main.js
@@ -0,0 +1,8 @@
+import { Meteor } from 'meteor/meteor';
+
+Meteor.startup(() => {
+  // code to run on server at startup
+});
+
+import '../imports/db.js';
+console.log(<nome_da_coleção>.find().count());
