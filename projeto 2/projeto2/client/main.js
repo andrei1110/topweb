627df9cce67dd8718878a68d66660014438cc58e
andrei1110 <actoledo.andrei@gmail.com>
2017-07-05 21:48:00 -0300

    modificações de estilos

--- projeto 2/projeto2/client/main.js
+++ projeto 2/projeto2/client/main.js
@@ -4,46 +4,14 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import './main.html';
 import '../imports/banco.js';
 
-var acessoriesData = [
-	{
-		src: "img/acessorio1.fw.png",
-		title: "Mousepad Razer",
-		alt: "Mousepad Razer Goliathus verde",
-		descr: "Mousepad Razer Goliathus verde",
-	},
-	{
-		src: "img/acessorio2.fw.png",
-		title: "Mousepad HyperX",
-		alt: "Mousepad HyperX preto gigante que cobre toda a mesa",
-		descr: "Mousepad HyperX preto gigante",
-	},
-	{
-		src: "img/acessorio3.fw.png",
-		title: "Mouse Bungee Razer",
-		alt: "Mouse Bungee Razer preto",
-		descr: "Mouse Bungee Razer preto",
-	},
-	{
-		src: "img/acessorio4.fw.png",
-		title: "Volante Logitech",
-		alt: "Volante Logitech com dois eixos, 3 pedais, cambio e freio de mão",
-		descr: "Volante Logitech G29",
-	},
-	{
-		src: "img/acessorio5.png",
-		title: "Joystick Razer",
-		alt: "Joystick Razer iluminado com LED",
-		descr: "Joystick Razer iluminado com LED",
-	}
-];
-
+//Importação das imagens do banco
 Template.showMouse.helpers({img_data:mouse_img.find()});
 Template.showKeyboard.helpers({img_data:keyboard_img.find()});
 Template.showHs.helpers({img_data:hs_img.find()});
 Template.showAcessories.helpers({img_data:acessories_img.find()});
 
 
-
+//Modal
 var mouseSource = $("#modal").html();
 var geraTemplateModal = Template.geraModal.helpers(mouseSource);
 function displayModalMouse(event){
@@ -52,3 +20,8 @@ function displayModalMouse(event){
 	$("#modal-container").html(html);
 	$("#imageModal").modal('show');
 }
+
+//Usuario
+Accounts.ui.config({
+    passwordSignupFields: "USERNAME_AND_EMAIL"
+});
--- projeto 2/projeto2/server/main.js
+++ projeto 2/projeto2/server/main.js
@@ -1,6 +1,5 @@
 import { Meteor } from 'meteor/meteor';
 import '../imports/banco.js';
-console.log(mouse_img.find().count());
 
 Meteor.startup(() => {
   // code to run on server at startup
